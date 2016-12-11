617da2bfb941375a7a255b0165d354ccaccf42a4
joe-re <joe.tialtngo@gmail.com>
2016-12-12 02:17:42 +0900

    remove sync API
    couldn't do it.because click events are executed asynchronously on navive layer.

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const path = require('path');
+let _app = null;
 
 function apply(app) {
-  app.args.unshift(path.join(__dirname, 'preload.js'));
-  app.args.unshift('--require');
-  return app;
+  _app = app;
+  _app.args.unshift(path.join(__dirname, 'preload.js'));
+  _app.args.unshift('--require');
+  return _app;
 }
 
-function clickMenu(app, ...labels) {
-  app.electron.ipcRenderer.send('SPECTRON_FAKE_MENU/SEND', labels);
+function clickMenu(...labels) {
+  _app.electron.ipcRenderer.send('SPECTRON_FAKE_MENU/SEND', labels);
 }
 
 module.exports = { apply, clickMenu };
